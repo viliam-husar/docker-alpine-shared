7ed85607d7edcc07206e0a98344afcf36c10234e
Viliam Husar <solzenic@symfony.sk>
2017-06-12 17:30:39 +0100

    Removed build dependencies.

--- install-glibc-v1.sh
+++ install-glibc-v1.sh
@@ -1,6 +1,5 @@
 GLIBC_BASE_URL="https://github.com/sgerrand/alpine-pkg-glibc/releases/download" \
 && GLIBC_PACKAGE_VERSION="2.25-r0" \
-&& apk add --no-cache wget ca-certificates \
 && wget \
     "https://raw.githubusercontent.com/andyshinn/alpine-pkg-glibc/master/sgerrand.rsa.pub" \
     -O "/etc/apk/keys/sgerrand.rsa.pub" \
@@ -11,7 +10,7 @@ GLIBC_BASE_URL="https://github.com/sgerrand/alpine-pkg-glibc/releases/download" 
 && apk add --no-cache /tmp/glibc_packages/*.apk \
 && /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 C.UTF-8 || true \
 && echo "export LANG=C.UTF-8" > /etc/profile.d/locale.sh \
-&& apk del glibc-i18n wget ca-certificates \
+&& apk del glibc-i18n \
 && rm -rf /tmp/glibc_packages /root/.wget-hsts /etc/apk/keys/sgerrand.rsa.pub
 
 ENV LANG=C.UTF-8
