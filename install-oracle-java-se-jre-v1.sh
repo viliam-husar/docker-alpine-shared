047f27acda56737b07ae2e3b3f525b3ccdcbe401
Viliam Husar <solzenic@symfony.sk>
2017-08-03 10:31:30 +0200

    Updated Java Version

--- install-oracle-java-se-jre-v1.sh
+++ install-oracle-java-se-jre-v1.sh
@@ -3,12 +3,12 @@
 
 
 JAVA_VERSION=8
-JAVA_UPDATE=131
-JAVA_BUILD=11
+JAVA_UPDATE=144
+JAVA_BUILD=01
 JAVA_HOME="/opt/java"
 
 wget -nv -P "/tmp/java_packages/" --header "Cookie: oraclelicense=accept-securebackup-cookie;" \
-    "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}u${JAVA_UPDATE}-b${JAVA_BUILD}/d54c1d3a095b4ff2b6607d096fa80163/jre-${JAVA_VERSION}u${JAVA_UPDATE}-linux-x64.tar.gz"
+    "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION}u${JAVA_UPDATE}-b${JAVA_BUILD}/090f390dda5b47b9b721c7dfaa008135/jre-${JAVA_VERSION}u${JAVA_UPDATE}-linux-x64.tar.gz"
 tar -xzf "/tmp/java_packages/jre-${JAVA_VERSION}u${JAVA_UPDATE}-linux-x64.tar.gz" -C "/tmp/java_packages/"
 mkdir -p /opt
 mv "/tmp/java_packages/jre1.${JAVA_VERSION}.0_${JAVA_UPDATE}" "${JAVA_HOME}"
